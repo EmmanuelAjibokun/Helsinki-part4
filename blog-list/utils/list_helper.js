cc3b88743c3f472885d444060c3011e4d6b8d8b5
EmmanuelAjibokun <emmanuelajibokun9@gmail.com>
2025-11-19 13:24:48 +0100

    4.5: Helper Functions and Unit Tests, step 3

--- blog-list/utils/list_helper.js
+++ blog-list/utils/list_helper.js
@@ -10,7 +10,21 @@ const totalLikes = (blogs) => {
     return reducer;
 }
 
+const favoriteBlog = (blogs) => {
+    // update blog with the current highest likes
+    let highestLikes = 0;
+    for (let i = 0; i < blogs.length; i++) {
+        const element = blogs[i];
+        if (highestLikes < blogs[i].likes) {
+            highestLikes = blogs[i].likes;
+            continue;
+        }
+    }
+    const favorite = blogs.filter(item => item.likes === highestLikes)
+    return favorite[0];
+}
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
